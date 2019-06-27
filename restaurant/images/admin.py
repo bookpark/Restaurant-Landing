867af931ebc00885f5571d9327916756c5c5ee57
Booki Park <bkbkgg@gmail.com>
2019-06-27 14:19:48 +0900

    class명 변경 및 admin 추가

--- restaurant/images/admin.py
+++ restaurant/images/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from images.models import MainBanner
+
+admin.site.register(MainBanner)
--- restaurant/images/models.py
+++ restaurant/images/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 
-class Main(models.Model):
+class MainBanner(models.Model):
     image = models.ImageField(upload_to='main_banner')
