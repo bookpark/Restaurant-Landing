3ba9cae781632a45b6d82e4d98f7e0d7b07ecd1a
Booki Park <bkbkgg@gmail.com>
2019-06-27 18:13:43 +0900

    이미지 출력을 위한 views, url, template 추가 및 수정

--- restaurant/config/urls.py
+++ restaurant/config/urls.py
@@ -1,6 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from images.views import index, main_banner
+
 urlpatterns = [
+    path('', index, name='index'),
+    path('main/<int:pk>/', main_banner, name='main-banner'),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
--- restaurant/images/views.py
+++ restaurant/images/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from images.models import MainBanner
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def main_banners(request):
+    banners = MainBanner.objects.all()
+    context = {
+        'banners': banners,
+    }
+    return render(request, 'index.html', context)
+
+
+def main_banner(request, pk):
+    banner = get_object_or_404(MainBanner, pk=pk)
+    context = {
+        'banner': banner,
+    }
+    return render(request, 'index.html', context)
