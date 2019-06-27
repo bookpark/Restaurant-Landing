5ee808734b6fd06789b00b89db31f8bbc0476612
Booki Park <bkbkgg@gmail.com>
2019-06-27 14:45:04 +0900

    모델 추가 및 매직메서드 변경

--- restaurant/images/admin.py
+++ restaurant/images/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from images.models import MainBanner
+from images.models import MainBanner, BrandImage, BrandBanner, Menu
 
 admin.site.register(MainBanner)
+admin.site.register(BrandImage)
+admin.site.register(BrandBanner)
+admin.site.register(Menu)
--- restaurant/images/models.py
+++ restaurant/images/models.py
@@ -2,4 +2,29 @@ from django.db import models
 
 
 class MainBanner(models.Model):
-    image = models.ImageField(upload_to='main_banner')
+    main = models.ImageField(upload_to='main_banner')
+
+    def __str__(self):
+        return f'메인 배너 - [{self.main.name}]'
+
+
+class BrandImage(models.Model):
+    brand = models.ImageField(upload_to='main_brand')
+
+    def __str__(self):
+        return f'브랜드 페이지 - [{self.brand.name}]'
+
+
+class BrandBanner(models.Model):
+    brand_banner_1 = models.ImageField(upload_to='brand_banner_1')
+    brand_banner_2 = models.ImageField(upload_to='brand_banner_2')
+
+    def __str__(self):
+        return f'브랜드 배너 (이미지 두장) - [{self.brand_banner_1.name}, {self.brand_banner_2.name}]'
+
+
+class Menu(models.Model):
+    menu = models.ImageField(upload_to='menu')
+
+    def __str__(self):
+        return f'메뉴 - [{self.menu.name}]'
