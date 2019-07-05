86634f897e948632773fe54b738811860633e6ab
Booki Park <bkbkgg@gmail.com>
2019-07-05 11:49:39 +0900

    기능 임시 삭제

--- restaurant/config/urls.py
+++ restaurant/config/urls.py
@@ -3,11 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from images.views import index, main_banner
+from images.views import index
 
 urlpatterns = [
     path('', index, name='index'),
-    path('main/<int:pk>/', main_banner, name='main-banner'),
     path('admin/', admin.site.urls),
 ]
 
